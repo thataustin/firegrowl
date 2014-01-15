370044ac6a7995d72c4d1edabac8b84fc54d7f67
Austin Brown <thataustin@gmail.com>
2014-01-15 01:53:19 -0800

    Refactoring eol, tab space sequence and object path display (using join)

--- test.js
+++ test.js
@@ -5,19 +5,19 @@ var fbURL = null; // Set your test url here (best with now read/write rules)
 
 if (!fbURL) { throw 'Must set unregulated FB url to run this test!'; }
 
-firegrowl(fbURL); // this call will begin a connection that only listens
+firegrowl(fbURL, {sticky: true}); // this call will begin a connection that only listens
 
 // create our own local connection to make the updates with
 var Firebase = require('firebase');
 
 var fb = new Firebase(fbURL);
 
-console.log('should get a growl notification about doctor_name being added first');
+console.log('should get a growl notification about doctor name/enemy first');
 setTimeout(function() {
-  fb.set({'doctor_name': 'Doctor Who'});
+  fb.set({'doctor_name': 'Doctor Who', 'doctor_enemy': 'Dalek'});
 }, 1000);
 
-console.log('should get a growl notification about doctor name being updated second');
+console.log('should get a growl notification about doctor_name being updated second');
 setTimeout(function() {
-  fb.set({'doctor_name': 'Dr Who'});
+  fb.set({'doctor_name': 'Dr Who', 'doctor_enemy': 'Cybermen'});
 }, 2000);
